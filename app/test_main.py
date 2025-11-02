e09b0b8e5830927e3ce51a6a25ac725cf66e90ee
Yevhen Dmytrenko <zdmitrenko26@gmail.com>
2025-11-02 23:51:18 +0200

    Add all tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,33 @@
-# write your code here
+import pytest
+
+from app.main import check_password
 
 
-def test_check_password() -> None:
-    pass
+@pytest.mark.parametrize(
+    "password,result",
+    [
+        ("Pass@word1", True),
+        ("Qwertyqwerty", False),
+        ("Str@ngstrong", False),
+        ("str@ngstrong1", False),
+        ("Stringstrong1", False),
+        ("St@rin1", False),
+        ("St@rin1St@rin1St@rin1", False),
+        ("St@rin1стрінг", False),
+
+    ],
+    ids=[
+        "valid_password",
+        "pass_without_digit_and_symbol",
+        "pass_without_digit",
+        "pass_without_uppercase_letter",
+        "pass_without_symbol",
+        "too_small_pass",
+        "too_big_pass",
+        "pass_with_cyrilic_symbols",
+    ]
+)
+def test_check_valid_password(password, result) -> None:
+    assert (
+        check_password(password) == result
+    ), f"Check_password with password={password} should return {result}!"
